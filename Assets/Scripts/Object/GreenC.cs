f5df4799d2467b59a07d79f2d5e8f885a84f0807
lsheon0728 <112855993+lsheon0728@users.noreply.github.com>
2023-12-01 18:30:03 +0900

    Add files via upload
    GameManager, GreenGem, BlueGem, Key 구현했습니다. 231201

---
+++ Assets/Scripts/Object/GreenC.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenC : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public CircleCollider2D Col;
+    public Vector2 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Vector2.right / 150.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += (Vector3) speed;
+    }
+}
